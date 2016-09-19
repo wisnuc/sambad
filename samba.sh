7870e0bf5a5622048b5867cd3bf51dced45dac76
John <wei.jiang@winsuntech.cn>
2016-09-19 15:56:36 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 set -e                              # should exit if any statement returns a non-true return value
-set -o nounset                      # Treat unset variables as an error
-SHELL="/home/test.sh"
 
-source $SHELL
-if [ $? -eq 0 ];then
-    ionice -c 3 nmbd -D && ionice -c 3 smbd -D
-    while true; do sleep 1000; done
-else
-    exit 100
-fi
+chmod a+x /bootstrap.sh
+/bootstrap.sh
+ionice -c 3 nmbd -D && ionice -c 3 smbd -D
+
+while true; do sleep 1000; done
