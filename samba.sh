ebf54e771f5cc483e13de75ac0d28e48c71917a1
John <wei.jiang@winsuntech.cn>
2016-09-19 18:22:18 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -4,6 +4,8 @@ set -e                              # should exit if any statement returns a non
 
 chmod a+x /bootstrap.sh
 /bootstrap.sh
+
+service rsyslog restart
 ionice -c 3 nmbd -D && ionice -c 3 smbd -D
 
 while true; do sleep 1000; done
