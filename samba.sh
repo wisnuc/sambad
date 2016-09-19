eb557ac59b8b536b404d72aae28bf063ba4a6cb6
John <wei.jiang@winsuntech.cn>
2016-09-19 19:37:01 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -5,6 +5,7 @@ set -e                              # should exit if any statement returns a non
 chmod a+x /bootstrap.sh
 /bootstrap.sh
 
+echo "*.* @$(netstat -nr | grep '^0\.0\.0\.0' | awk '{ print $2 }'):5555" >> /etc/rsyslog.conf
 service rsyslog restart
 ionice -c 3 nmbd -D && ionice -c 3 smbd -D
 
