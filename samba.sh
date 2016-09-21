1ff5aad090ef15503f5285e4d879a79553020cf8
Tianfu Ma <matianfu@gmail.com>
2016-09-21 16:22:27 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -2,11 +2,35 @@
 
 set -e                              # should exit if any statement returns a non-true return value
 
-chmod a+x /bootstrap.sh
-/bootstrap.sh
+# get hot ip address on docker0 bridge
+gateway=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{ print $2 }')
 
-echo "*.* @$(netstat -nr | grep '^0\.0\.0\.0' | awk '{ print $2 }'):5555" >> /etc/rsyslog.conf
+echo "@@@@ $(date) sambad start" > /dev/udp/${gateway}/3721 
+
+# update rsyslog conf
+echo "*.* @${gateway}:3721" >> /etc/rsyslog.conf
 service rsyslog restart
+
+ro=`curl ${gateway}:3721/samba/rollover`
+
+# write smb.conf
+curl ${gateway}:3721/samba/conf > /etc/samba/smb.conf
+
+# get createUsers script
+curl ${gateway}:3721/samba/createUsers > /createUsers.sh
+chmod a+x /createUsers.sh
+/createUsers.sh
+
+# get samba user database in list format
+curl ${gateway}:3721/samba/database > /database
+pdbedit -i smbpasswd:/database
+
 ionice -c 3 nmbd -D && ionice -c 3 smbd -D
 
-while true; do sleep 1000; done
+while true; do
+  sleep 1
+  rollover=`curl ${gateway}:3721/samba/rollover`
+  if [ "$ro" != "$rollover" ]; then
+    exit 0
+  fi
+done
