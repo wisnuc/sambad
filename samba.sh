723a26b83da341438ae13370b5ce108d33124b8d
Tianfu Ma <matianfu@gmail.com>
2016-09-21 17:13:30 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -5,10 +5,10 @@ set -e                              # should exit if any statement returns a non
 # get hot ip address on docker0 bridge
 gateway=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{ print $2 }')
 
-echo "@@@@ $(date) sambad start" > /dev/udp/${gateway}/3721 
+echo -n "@@@@ $(date) sambad start" > /dev/udp/${gateway}/3721 
 
 # update rsyslog conf
-echo "*.* @${gateway}:3721" >> /etc/rsyslog.conf
+echo "local7.* @${gateway}:3721" >> /etc/rsyslog.conf
 service rsyslog restart
 
 ro=`curl ${gateway}:3721/samba/rollover`
