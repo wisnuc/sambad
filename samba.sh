ad27ddea80a11f6fe120ee453cd7c29da14c86f2
Tianfu Ma <matianfu@gmail.com>
2016-09-22 15:57:55 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -4,6 +4,11 @@ set -e                              # should exit if any statement returns a non
 
 sleep 5
 
+# get hot ip address on docker0 bridge
+gateway=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{ print $2 }')
+
+echo -n "@@@@ $(date) sambad start" > /dev/udp/${gateway}/3721
+
 # remove existing user with 5000 > uid >= 2000
 while read line; do
 
@@ -12,19 +17,14 @@ while read line; do
 
   if [ "$USERUID" -ge "2000" ] && [ "$USERUID" -lt "5000" ]; then
     deluser $USERNAME
-    echo -n "@@@@ $(date) user $USERNAME deleted"
+    echo -n "@@@@ $(date) user $USERNAME deleted" > /dev/udp/${gateway}/3721
   fi  
 
 done < /etc/passwd
 
 # remove existing tdb
 rm -rf /var/lib/samba/private/passdb.tdb
-echo -n "@@@@ $(date) samba passdb.tdb removed"
-
-# get hot ip address on docker0 bridge
-gateway=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{ print $2 }')
-
-echo -n "@@@@ $(date) sambad start" > /dev/udp/${gateway}/3721 
+echo -n "@@@@ $(date) samba passdb.tdb removed" > /dev/udp/${gateway}/3721
 
 # update rsyslog conf
 echo "local7.* @${gateway}:3721" >> /etc/rsyslog.conf
@@ -35,17 +35,21 @@ echo -n "@@@@ $(date) init rollover ${ro}" > /dev/udp/${gateway}/3721
 
 # write smb.conf
 curl ${gateway}:3721/samba/conf > /etc/samba/smb.conf
+echo -n "@@@@ $(date) smb.conf written" > /dev/udp/${gateway}/3721
 
 # get createUsers script
 curl ${gateway}:3721/samba/createUsers > /createUsers.sh
 chmod a+x /createUsers.sh
 /createUsers.sh
+echo -n "@@@@ $(date) users created" > /dev/udp/${gateway}/3721
 
 # get samba user database in list format
 curl ${gateway}:3721/samba/database > /database
 pdbedit -i smbpasswd:/database
+echo -n "@@@@ $(date) samba passwd database updated" > /dev/udp/${gateway}/3721
 
 ionice -c 3 nmbd -D && ionice -c 3 smbd -D
+echo -n "@@@@ $(date) smbd and nmbd started" > /dev/udp/${gateway}/3721
 
 while true; do
   sleep 5
